a7987e1e7fc9e6059d3f34b82a76eb4bc29c6776
Lucas Magno <lucasmagnorc@gmail.com>
2020-08-14 22:14:21 -0300

    Add main script

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  // Read websites file
+  const fs = require('fs');
+  const contentFile = fs.readFileSync('website_list.txt', 'utf8');
+  const arrayWebSite = contentFile.split('\n');
+
+  while(true){
+    // Open browser
+    const browser = await puppeteer.launch({ headless: true });
+
+    for(let i = 0; i < arrayWebSite.length; i++){
+      // Open a new page
+      const page = await browser.newPage();
+      await page.waitFor(5000);
+
+      // Redirect to page
+      await page.goto(arrayWebSite[i]);
+      await page.waitFor(10000);
+
+      // Close page
+      await page.close();
+      console.log("Visited: " + arrayWebSite[i]);
+      await page.waitFor(8000);
+    }
+  }
+})();
